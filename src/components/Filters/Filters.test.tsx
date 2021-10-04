a23fb960d824a27c38850f3f6042195e5733b7dc
Fernando Medeiros do Nascimento <fernando.nascimento@MacBook-Pro-de-Fernando.local>
2021-10-04 18:24:12 -0300

    Fernando - Desenvolver Layout - Fixing error on Filters.test.tsx

--- src/components/Filters/Filters.test.tsx
+++ src/components/Filters/Filters.test.tsx
@@ -2,8 +2,11 @@ import { render, screen } from '@testing-library/react';
 import Filters from './Filters';
 
 describe('<Filters/>', () => {
+  const firstFilter = '08/09/20';
+  const secondFilter = '08/09/21';
+
   beforeEach(() => {
-    render(<Filters />);
+    render(<Filters firstFilter={firstFilter} secondFilter={secondFilter} />);
   });
 
   it('Should render the TimeFilter component', () => {
