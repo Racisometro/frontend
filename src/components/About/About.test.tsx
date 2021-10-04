86022bd2a6fc5a38b999b593451c8488c5c0aa6a
Fernando Medeiros do Nascimento <fernando.nascimento@MacBook-Pro-de-Fernando.local>
2021-10-04 18:44:30 -0300

    Fernando - Desenvolver LAyout - Updating About.test.tsx to fix the pipeline

--- src/components/About/About.test.tsx
+++ src/components/About/About.test.tsx
@@ -1,3 +1,10 @@
+import { render } from '@testing-library/react';
+import About from './About';
+
 describe('<About/>', () => {
+  beforeEach(() => {
+    render(<About />);
+  });
+
   it('Should display the block title', () => {});
 });
